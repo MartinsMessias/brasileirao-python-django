a29893fbf0f2c6de3804d14d14df01fa1cdd981f
MartinsMessias <messiasads2017@gmail.com>
2020-11-02 23:20:21 -0500

    Criação do template base e extendendo aos outros templates

--- brasileirao/urls.py
+++ brasileirao/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core_app.views import Classificacao, InformacoesCampeonato, Rodadas, Rodada, Artilharia
+from core_app.views import Classificacao, InformacoesCampeonato, Rodadas, Rodada
 
 urlpatterns = [
     path('admin/', admin.site.urls),
