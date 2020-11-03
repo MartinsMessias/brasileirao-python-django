c66a5d4a4189441b2fbb2cfaf7f569ef23300c84
MartinsMessias <messiasads2017@gmail.com>
2020-11-02 21:53:46 -0500

    Exibindo a classificação atual

--- brasileirao/urls.py
+++ brasileirao/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core_app.views import Home
+from core_app.views import Home, Classificacao
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Home.as_view(), name='homepage')
+    path('', Home.as_view(), name='homepage'),
+    path('classificacao/', Classificacao.as_view(), name='classificacao')
 ]
--- core_app/views.py
+++ core_app/views.py
@@ -4,7 +4,18 @@ from django.shortcuts import render
 from django.views import View
 
 requests_cache.install_cache('api_call_cache', backend='sqlite', expire_after=180)
-API_TOKEN = 'test_507a1b98973ec201a987732daf6241'
+API_TOKEN = 'live_64d523ff143f84a749cc4b6d30bf70'
+
+
+class Classificacao(View):
+    template_name = 'classificacao_brasileirao.html'
+
+    def get(self, request):
+        url = 'https://api.api-futebol.com.br/v1/campeonatos/10/tabela'
+        headers = {'Authorization': f'Bearer {API_TOKEN}'}
+        context = {'data': requests.get(url, headers=headers).json()}
+
+        return render(request, self.template_name, context)
 
 
 class Home(View):
